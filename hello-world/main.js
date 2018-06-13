92159c28e9e16166b6c3881564566c4bb5e33b2e
GekiSoft <GekiSoft@gmail.com>
2018-06-13 16:50:30 +0800

    backing up

--- hello-world/main.js
+++ hello-world/main.js
@@ -7,14 +7,23 @@ let win
 function createWindow() {
     win = new BrowserWindow({
         width : 800,
-        height : 600
+        height : 600,
+        show: false
     })
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }))
+    win.once('ready-to-show', () => {
+        win.show()
+    })
 }
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+    createWindow()
+})
 
+app.on('window-all-closed', () => {
+    app.quit()
+})
